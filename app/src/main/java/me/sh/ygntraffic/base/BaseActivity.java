34d09b70fb59ac5e515db9b86a16b947bd2bb7a4
SH <swanhtet1992@gmail.com>
2014-06-21 00:36:16 +0630

    Add report function in DetailActivity

--- app/src/main/java/me/sh/ygntraffic/base/BaseActivity.java
+++ app/src/main/java/me/sh/ygntraffic/base/BaseActivity.java
@@ -1,5 +1,6 @@
 package me.sh.ygntraffic.base;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
@@ -9,9 +10,12 @@ import me.sh.ygntraffic.R;
  * Created by SH on 16/Jun/2014.
  */
 public class BaseActivity extends ActionBarActivity {
+  protected Context context;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    context = this.getApplicationContext();
     overridePendingTransition(R.anim.activity_open_translate, R.anim.activity_close_scale);
   }
 
--- app/src/main/java/me/sh/ygntraffic/ui/DetailActivity.java
+++ app/src/main/java/me/sh/ygntraffic/ui/DetailActivity.java
@@ -1,19 +1,33 @@
 package me.sh.ygntraffic.ui;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import me.sh.ygntraffic.R;
+import me.sh.ygntraffic.base.BaseActivity;
 import me.sh.ygntraffic.model.Place;
+import me.sh.ygntraffic.util.NetUtil;
 
-public class DetailActivity extends ActionBarActivity {
+public class DetailActivity extends BaseActivity {
 
   public static final String PLACE = "place";
   private Place mPlace;
+  private ProgressDialog mDialog;
 
   @InjectView(R.id.traffic_name)
   TextView trafficName;
@@ -30,13 +44,115 @@ public class DetailActivity extends ActionBarActivity {
 
     setContentView(R.layout.activity_detail);
     ButterKnife.inject(this);
-
+    mDialog = new ProgressDialog(this);
     if (!getIntent().getExtras().isEmpty()) {
       mPlace = (Place) getIntent().getExtras().get(PLACE);
       trafficRemark.setText(mPlace.getStatus());
       trafficRemark.setBackgroundColor(mPlace.getStatusColor(this, mPlace.getStatus()));
       trafficName.setText(mPlace.getName());
       trafficReportTime.setText(mPlace.getReportedTime());
+    }
+  }
+
+  @OnClick(R.id.btn_report)
+  public void reportTraffic() {
+    createStatusDialog();
+  }
+
+  private void createStatusDialog() {
+    final AlertDialog.Builder builderSingle = new AlertDialog.Builder(this);
+    builderSingle.setTitle(mPlace.getName());
+    final String[] status = getResources().getStringArray(R.array.report_status);
+    final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, status);
+
+    builderSingle.setNegativeButton("Dismiss",
+        new DialogInterface.OnClickListener() {
+
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+          }
+        }
+    );
+
+    builderSingle.setAdapter(arrayAdapter,
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            if (which == 0) {
+              createRemarkDialog("3", status[which]);
+            } else if (which == 1) {
+              createRemarkDialog("2", status[which]);
+            } else if (which == 2) {
+              reportToServer("1", "");
+            }
+          }
+        }
+    );
+    builderSingle.show();
+  }
+
+  private void createRemarkDialog(final String id, String title) {
+    final AlertDialog.Builder builderSingle = new AlertDialog.Builder(this);
+    builderSingle.setTitle(title);
+    String[] remark = getResources().getStringArray(R.array.report_remark);
+    final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, remark);
+
+    builderSingle.setNegativeButton("Dismiss",
+        new DialogInterface.OnClickListener() {
+
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+          }
+        }
+    );
+
+    builderSingle.setAdapter(arrayAdapter,
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            int remark = which + 1;
+            reportToServer(id, remark + "");
+          }
+        }
+    );
+    builderSingle.show();
+  }
+
+  private void reportToServer(final String status, String remark) {
+    if (NetUtil.isOnline(context)) {
+      mDialog.setMessage(getString(R.string.loading_report));
+      mDialog.show();
+      Ion.with(context)
+          .load("http://cyantra.co/api.php?r=update&p=" + mPlace.getName() + "&s=" + status + "&c=" + remark)
+          .setLogging("ION-ReportTraffic", Log.DEBUG)
+          .asString()
+          .setCallback(new FutureCallback<String>() {
+            @Override
+            public void onCompleted(Exception e, String result) {
+              mDialog.dismiss();
+              Log.d("", "Result: " + result);
+              if (result.equalsIgnoreCase("") || result == null) {
+                mDialog.setMessage(getString(R.string.error_reprot));
+                mDialog.show();
+                mDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                  @Override
+                  public void onCancel(DialogInterface dialog) {
+                    finish();
+                  }
+                });
+              } else {
+                JsonParser parser = new JsonParser();
+                JsonObject jsonObject = (JsonObject) parser.parse(result);
+                Toast.makeText(context, "Reported: " + jsonObject.get("remark"), Toast.LENGTH_SHORT).show();
+              }
+            }
+          });
+    } else {
+      Toast.makeText(context, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
     }
   }
 
