3c28c27af9138a5bb0c8a648155fc3bfdf5851c8
SH <swanhtet1992@gmail.com>
2014-06-21 01:03:08 +0630

    Add API URL as Constants

---
+++ app/src/main/java/me/sh/ygntraffic/util/Constants.java
@@ -0,0 +1,17 @@
+package me.sh.ygntraffic.util;
+
+/**
+ * Created by SH on 21/Jun/2014.
+ */
+public class Constants {
+  public static final String BASE_URL = "http://cyantra.co";
+  public static final String ALL_TRAFFIC = BASE_URL + "/api/get";
+  public static final String RECENT_TRAFFIC = BASE_URL + "/api/get/recent";
+  public static final String BAD_TRAFFIC = BASE_URL + "/api/get/bad";
+  public static final String NORMAL_TRAFFIC = BASE_URL + "/api/get/normal";
+  public static final String MY_PLACE = "/api/get/mine?i=";
+  public static final String PLACES = BASE_URL + "/api/get/places";
+  public static final String ONE_PLACE = BASE_URL + "/api/search?p=";
+  public static final String REPORT = BASE_URL + "/api/update?p=";
+  public static final String FLAG = BASE_URL + "/api/flag?p=";
+}
