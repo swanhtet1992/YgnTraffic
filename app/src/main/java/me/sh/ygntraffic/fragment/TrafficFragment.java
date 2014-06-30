3e5c630b39c0b49e26fa63319d348ffe1bbb3600
SH <swanhtet1992@gmail.com>
2014-07-01 00:10:31 +0630

    Implement report function. (Will refactor later)

--- app/src/main/java/me/sh/ygntraffic/fragment/TrafficFragment.java
+++ app/src/main/java/me/sh/ygntraffic/fragment/TrafficFragment.java
@@ -1,6 +1,9 @@
 package me.sh.ygntraffic.fragment;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
@@ -10,8 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -25,6 +30,7 @@ import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import me.sh.ygntraffic.R;
 import me.sh.ygntraffic.adapter.PlaceAdapter;
 import me.sh.ygntraffic.base.BaseFragment;
@@ -35,6 +41,8 @@ import me.sh.ygntraffic.util.NetUtil;
 public class TrafficFragment extends BaseFragment implements AbsListView.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener {
 
   private OnPlaceClickListener mListener;
+  private ProgressDialog mDialog;
+  private String mPlace;
 
   /**
    * The fragment's ListView/GridView.
@@ -76,9 +84,11 @@ public class TrafficFragment extends BaseFragment implements AbsListView.OnItemC
     View view = inflater.inflate(R.layout.fragment_place, container, false);
     ButterKnife.inject(this, view);
 
+    mDialog = new ProgressDialog(getActivity());
+
     mSwipeRefreshLayout.setOnRefreshListener(this);
-    mSwipeRefreshLayout.setColorScheme(R.color.yellow, R.color.red,
-        R.color.green, R.color.red);
+    mSwipeRefreshLayout.setColorScheme(R.color.yellow, R.color.white,
+        R.color.yellow, R.color.white);
 
     // Set OnItemClickListener so we can be notified on item clicks
     mListView.setOnItemClickListener(this);
@@ -145,7 +155,6 @@ public class TrafficFragment extends BaseFragment implements AbsListView.OnItemC
         });
   }
 
-
   @Override
   public void onRefresh() {
     if (NetUtil.isOnline(context)) {
@@ -177,6 +186,172 @@ public class TrafficFragment extends BaseFragment implements AbsListView.OnItemC
       }
     }
     return super.onOptionsItemSelected(item);
+  }
+
+  /**
+   * TODO Need to refactor this Reporting part
+   */
+  @OnClick(R.id.btn_report)
+  public void reportTraffic() {
+    mDialog.setMessage("Loading places...");
+    mDialog.show();
+    Ion.with(getActivity())
+        .load(Constants.PLACES)
+        .setLogging("ION", Log.DEBUG)
+        .asString()
+        .setCallback(new FutureCallback<String>() {
+          @Override
+          public void onCompleted(Exception e, String result) {
+            mDialog.dismiss();
+            Log.e("", "Result: " + result);
+            if (result == null || e != null) {
+              setEmptyText(getText(R.string.error_internet));
+            } else {
+              JsonParser parser = new JsonParser();
+              JsonArray array = (JsonArray) parser.parse(result);
+              String[] places = new String[array.size()];
+              for (int i = 0; i < array.size(); i++) {
+                places[i] = array.get(i).getAsString();
+              }
+              createPlaceDialog(places);
+            }
+          }
+        });
+  }
+
+  private void createPlaceDialog(final String[] places) {
+    final AlertDialog.Builder builderSingle = new AlertDialog.Builder(getActivity());
+    builderSingle.setTitle("Choose place to report");
+    final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context,
+        R.layout.dialog_list_item, places);
+
+    builderSingle.setNegativeButton("Dismiss",
+        new DialogInterface.OnClickListener() {
+
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+          }
+        }
+    );
+
+    builderSingle.setAdapter(arrayAdapter,
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            mPlace = places[which];
+            createStatusDialog(places[which]);
+          }
+        }
+    );
+    builderSingle.show();
+  }
+
+  private void createStatusDialog(String place) {
+    final AlertDialog.Builder builderSingle = new AlertDialog.Builder(getActivity());
+    builderSingle.setTitle(place);
+    final String[] status = getResources().getStringArray(R.array.report_status);
+    final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context,
+        R.layout.dialog_list_item, status);
+
+    builderSingle.setNegativeButton("Dismiss",
+        new DialogInterface.OnClickListener() {
+
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+          }
+        }
+    );
+
+    builderSingle.setAdapter(arrayAdapter,
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            if (which == 0) {
+              createRemarkDialog("3", status[which]);
+            } else if (which == 1) {
+              createRemarkDialog("2", status[which]);
+            } else if (which == 2) {
+              reportToServer("1", "");
+            }
+          }
+        }
+    );
+    builderSingle.show();
+  }
+
+  private void createRemarkDialog(final String id, String title) {
+    final AlertDialog.Builder builderSingle = new AlertDialog.Builder(getActivity());
+    builderSingle.setTitle(title);
+    String[] remark = getResources().getStringArray(R.array.report_remark);
+    final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context,
+        R.layout.dialog_list_item, remark);
+
+    builderSingle.setNegativeButton("Dismiss",
+        new DialogInterface.OnClickListener() {
+
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+          }
+        }
+    );
+
+    builderSingle.setAdapter(arrayAdapter,
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            int remark = which + 1;
+            reportToServer(id, remark + "");
+          }
+        }
+    );
+    builderSingle.show();
+  }
+
+  private void reportToServer(final String status, String remark) {
+    if (NetUtil.isOnline(context)) {
+      mDialog.setMessage(getString(R.string.loading_report));
+      mDialog.show();
+      Ion.with(context)
+          .load(Constants.REPORT + mPlace + "&s=" + status + "&c=" + remark)
+          .setLogging("ION-ReportTraffic", Log.DEBUG)
+          .asString()
+          .setCallback(new FutureCallback<String>() {
+            @Override
+            public void onCompleted(Exception e, String result) {
+              mDialog.dismiss();
+              Log.d("", "Result: " + result);
+              if (result.equalsIgnoreCase("") || result == null) {
+                mDialog.setMessage(getString(R.string.error_reprot));
+                mDialog.show();
+                mDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                  @Override
+                  public void onCancel(DialogInterface dialog) {
+                    getActivity().finish();
+                  }
+                });
+              } else if (result.equalsIgnoreCase("{err: 3, msg: \"Bad request!\"}")) {
+                mDialog.setMessage(getString(R.string.error_reprot));
+                mDialog.show();
+                mDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                  @Override
+                  public void onCancel(DialogInterface dialog) {
+                    getActivity().finish();
+                  }
+                });
+              } else {
+                JsonParser parser = new JsonParser();
+                JsonObject jsonObject = (JsonObject) parser.parse(result);
+                Toast.makeText(context, "Reported: " + jsonObject.get("remark"), Toast.LENGTH_SHORT).show();
+                onRefresh();
+              }
+            }
+          });
+    } else {
+      Toast.makeText(context, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
+    }
   }
 
   /**
